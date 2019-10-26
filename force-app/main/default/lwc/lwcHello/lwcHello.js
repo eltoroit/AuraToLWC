05651269f2d6618dcfe6dce757a6893fa7fb4019
Andres Perez <andresperezf@yahoo.com>
2019-10-26 09:48:47 -0400

    Lightning button

--- force-app/main/default/lwc/lwcHello/lwcHello.js
+++ force-app/main/default/lwc/lwcHello/lwcHello.js
@@ -2,12 +2,8 @@ import { LightningElement, track } from 'lwc';
 
 export default class LwcHello extends LightningElement {
 	@track greeting = "world";
-	/*
-	({
-		count: function (component, event, helper) {
-			var greeting = component.get("v.greeting");
-			alert(greeting.length + " letters");
-		}
-	});
-	*/
+
+	count() {
+		alert(this.greeting.length + " letters");
+	}
 }
