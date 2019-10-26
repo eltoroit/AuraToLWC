b6a39a57c7d2f4e6f700c5a6e3cff072b556ef81
Andres Perez <andresperezf@yahoo.com>
2019-10-26 09:48:46 -0400

    Copy Aura into LWC

--- force-app/main/default/lwc/lwcHello/lwcHello.js
+++ force-app/main/default/lwc/lwcHello/lwcHello.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class LwcHello extends LightningElement {}
+export default class LwcHello extends LightningElement {
+	/*
+	({
+		count: function (component, event, helper) {
+			var greeting = component.get("v.greeting");
+			alert(greeting.length + " letters");
+		}
+	});
+	*/
+}
